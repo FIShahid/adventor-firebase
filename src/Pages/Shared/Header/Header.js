5e0fed1ef57aa386e456a8de41c83ac792f2b6de
Fokhrul Islam <shahidcse12@gmail.com>
2022-04-17 02:32:16 +0600

    navbar added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <header>
-            <h2>This is Header</h2>
-        </header>    
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home">Adventure Travelers</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="ms-auto">
+      <Nav.Link href="#services">Services</Nav.Link>
+      <Nav.Link href="#blogs">Blogs</Nav.Link>
+      <Nav.Link href="#aboutme">About Me</Nav.Link>
+      
+    </Nav>
+    <Nav>
+      <Nav.Link href="#login">Login</Nav.Link>
+      {/* <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link> */}
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>    
         
         );
 };
